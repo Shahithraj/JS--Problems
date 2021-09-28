13dd18c8f1c8ab016bd1d2c2d247f7dc4e38fecd
Shahith R <87928073+Shahithraj@users.noreply.github.com>
2021-09-28 20:53:32 +0530

    Update frequency_count.js

--- frequency_count.js
+++ frequency_count.js
@@ -1,53 +1,53 @@
 1.NESTED LOOP
-// function same(arr1,arr2){
-// if(arr1.length!==arr2.length){
-//  return false;
-// }
-//    for(let i=0;i<arr1.length;i++){
-// // index of b = what the index of the number. Eg: b.indexOf(4) in array b which is 1.
-//     let index_of_b = arr2.indexOf(arr1[i]**2) //it is also looping
-//       if(index_of_b === -1){
-//        return false;
-//   }
-//     arr2.splice(index_of_b,1)
-//   }
-//   return true;
+function same(arr1,arr2){
+if(arr1.length!==arr2.length){
+ return false;
+}
+   for(let i=0;i<arr1.length;i++){
+// index of b = what the index of the number. Eg: b.indexOf(4) in array b which is 1.
+    let index_of_b = arr2.indexOf(arr1[i]**2) //it is also looping
+      if(index_of_b === -1){
+       return false;
+  }
+    arr2.splice(index_of_b,1)
+  }
+  return true;
 
-//  }
-// console.log(same([1,2,3,4,6],[1,36,8,4,16]));
+ }
+console.log(same([1,2,3,4,6],[1,36,8,4,16]));
 
 
 2.O(n)
-// function same(arr1,arr2){
-//   if(arr1.length!==arr2.length){
-//     return false;
-//   }
-//   let result1 ={};
-//   let result2 ={};
-//   for(let value of arr1){
-//     result1[value] = ++result1[value] || 1;
-//   }
-//   for(let value of arr2){
-//     result2[value] = ++result2[value] || 1;
-//   }
-//   for(let key in result1){
-//     if(!(key ** 2 in result2)){
-//       return false;
-//     }
-//   }
-//   for(let key in result2){
-//     if(result2[key ** 2]!==result1[key]){
-//       return false;
-//     }
-//   }
-//     console.log(result1,result2)
-//   return true;
+function same(arr1,arr2){
+  if(arr1.length!==arr2.length){
+    return false;
+  }
+  let result1 ={};
+  let result2 ={};
+  for(let value of arr1){
+    result1[value] = ++result1[value] || 1;
+  }
+  for(let value of arr2){
+    result2[value] = ++result2[value] || 1;
+  }
+  for(let key in result1){
+    if(!(key ** 2 in result2)){
+      return false;
+    }
+  }
+  for(let key in result2){
+    if(result2[key ** 2]!==result1[key]){
+      return false;
+    }
+  }
+    console.log(result1,result2)
+  return true;
 
 // }
 
 // console.log(same([1,4,2,3,4],[1,4,16,9,16]))
 
-// 3.
+// 3.one object only
 function same(a,b){
     if(a.length!==b.length){
       return false
